569ecdc0b2e1cf8d63f2c65566e483ab5cd09f73
Khandoker Tajuddin <khtajuddin97@gmail.com>
2022-10-16 11:42:59 +0600

    class-20-code-part1

---
+++ app/Controllers/Student.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Project\Controllers;
+
+class Student{
+    public $id;
+    public $name;
+
+    public function __construct(){
+        session_start();
+    }
+
+    public function store($data){
+        // session_start();      
+        $_SESSION['students'][] = $data;
+        $_SESSION['message'] = 'Successfully Created';
+        }
+
+        public function details($id)
+        {
+            // echo '<pre>';
+            // print_r($_SESSION['students']);
+            
+        $students = $_SESSION['students'];
+        $studentInfo = null;
+        foreach ($students as $key => $student){
+        if($student['id'] == $id) {
+        $studentInfo = $_SESSION['students'][$key];
+    }
+}
+    return $studentInfo;
+
+    }
+
+    public function destroy($id){
+        $students = $_SESSION['students'];
+        foreach ($students as $key => $student) {
+            if($student['id'] == $id){
+                unset($_SESSION['students'][$key]);
+            }
+        }
+        $_SESSION['message'] = 'Successfully Deleted';
+    }
+
+
+
+    
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    session_start();
+    // include_once './store.php';
+    $students = $_SESSION['students'] ?? [];
+?>
+
+
+    <!-- <div style="width: 500px; margin: 0 auto">
+
+    <form action="./store.php"  method="post">
+     <input name="id" type="text" placeholder="Enter your ID"></br>
+     <input name="name" type="text" placeholder="Enter your name"></br>
+     <button>Add</button>
+    </form> -->
+
+    <?php 
+
+        if(isset($_SESSION['message'])){
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        }
+        echo $_SESSION['message'] ?? null;
+    ?>
+<a href="./create.php">Create</a>
+    <table border="1" style="width: 100%;" >
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <div>
+                
+            </div>
+        
+            <?php
+                foreach($students as $student){?>
+                <tr>
+                    <td><?php echo $student['id']?></td>
+                    <td><?php echo $student['name']?></td>
+                    <td>
+                        <!-- <a href="#">Edit</a>| -->
+                        <a href="show.php?id=<?=$student['id']?>">Show</a>|
+                        <a href="delete.php?id=<?=$student['id']?>">Delete</a>
+                    </td>
+                </tr>
+
+                <?php }
+            ?>
+        </tbody>
+    </table>
+    </div>
+</body>
+</html>
